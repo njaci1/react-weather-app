d8c9d99b2a3cfe30cb11bd6b34e45462db41eb1d
njaci1 <githatukelvin@gmail.com>
2023-06-24 00:29:04 +0300

    Non responsive version

--- src/App.js
+++ src/App.js
@@ -1,34 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const url =
-    'https://api.openweathermap.org/data/2.5/weather?q=mombasa&appid=c022d502f9399fce90cb300da543a6e0';
+  const [location, setLocation] = React.useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=c022d502f9399fce90cb300da543a6e0`;
+  const [data, setData] = React.useState({});
+  useEffect(() => {
+    const fetchWeatherByCoordinates = (latitude, longitude) => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=c022d502f9399fce90cb300da543a6e0`;
+
+      axios.get(url).then((res) => {
+        console.log(res.data.name);
+        setData(res.data);
+        setLocation(res.data.name);
+      });
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        fetchWeatherByCoordinates(latitude, longitude);
+      });
+    }
+  }, []);
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((res) => {
+        setData(res.data);
+      });
+    }
+  };
   return (
     <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyPress={searchLocation}
+        ></input>
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <h1 className="city">Mombasa</h1>
+            <h2 className="city">{location}</h2>
           </div>
           <div className="temp">
-            <h2 className="temp">30°C</h2>
+            {data.weather ? <h3 className="temp">{data.main.temp}°C</h3> : null}
           </div>
           <div className="description">
-            <p>Mawingu</p>
+            {data.weather ? <p>{data.weather[0].description}</p> : null}
           </div>
         </div>
         <div className="bottom">
           <div className="feels">
-            <p>40°C</p>
+            {data.weather ? <p>{data.main.feels_like}°C</p> : null}
+
             <p>Feels like</p>
           </div>
           <div className="humidity">
-            <p>80%</p>
+            {data.weather ? <p>{data.main.humidity}%</p> : null}
+
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p>10km/h</p>
+            {data.weather ? <p>{data.wind.speed}km/h</p> : null}
+
             <p>Wind speed</p>
           </div>
         </div>
